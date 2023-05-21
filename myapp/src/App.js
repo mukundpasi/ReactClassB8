3ce21a6d2323fb85c6e1719e93f2b1b85433d297
mukundpasi <mukundpasi.developer@gmail.com>
2023-05-21 18:50:35 +0530

    - WIP : Learning css in react

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hello from './component/Hello';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
       <Hello></Hello>
     </div>
   );
