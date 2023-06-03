e05c548b82f4aeae6cc6de6014eb5fad418edfc9
mukundpasi <mukundpasi.developer@gmail.com>
2023-06-03 18:29:31 +0530

    -useeffect dependcy array

--- myapp/src/component/LandingPage.js
+++ myapp/src/component/LandingPage.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function LandingPage() {
+    const [counter, setCounter] = useState(0)
   console.log("1111111111");
   useEffect(() => {
     console.log("i am in header");
     let body = document.querySelector(".header");
     console.log(body);
-  }, []);
+  }, [counter]);
 
   console.log("222222222222222");
 
-  return <div className="header">LandingPage</div>;
+  return <div className="header">LandingPage
+  <button onClick={()=>setCounter(counter + 1)}>+</button>
+  </div>;
+
+  
 }
